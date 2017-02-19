f744b7375254725ea409d48b328d5d9956796f66
Mantas Lukosevicius <man.lukosevicius@gmail.com>
2017-02-19 19:17:41 +0200

    added styled list of heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,16 @@ export class Hero {
   selector: 'app-root',
   template: `
   <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
+  
+  <h2>My Heroes</h2>
+  <ul class = "heroes">
+    <li *ngFor="let hero of heroes">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+      <!--each hero goes here -->
+    </li>
+  </ul>
+  
+  <h2>{{hero.name}} details:</h2>
   <div><label>id: </label>{{hero.id}}</div>
   <div>
     <label>name: </label>
@@ -20,8 +29,24 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Tour of heroes';
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'windstorm'
   };
 }
+
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
+
+
