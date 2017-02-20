62e516b79f4541022ce3d3b8ba22df01108144f4
Mantas Lukosevicius <man.lukosevicius@gmail.com>
2017-02-20 21:09:11 +0200

    added hero service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+
 
 @Component({
+  providers: [HeroService],
   selector: 'app-root',
   template: `
   <h1>{{title}}</h1>
@@ -33,10 +24,22 @@ const HEROES: Hero[] = [
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService){}
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
