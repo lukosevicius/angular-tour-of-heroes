fafdc14927546dfd20ce7368980df2ca29ce7260
Mantas Lukosevicius <man.lukosevicius@gmail.com>
2017-02-20 11:27:26 +0200

    transfered hero class and hero detail to separate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core';
+import {Hero} from './hero';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
 
 @Component({
   selector: 'app-root',
@@ -20,14 +29,7 @@ export class Hero {
     </li>
   </ul>
   
-  <div *ngIf = "selectedHero">
-    <h2>{{selectedHero.name}} details:</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>  
-  </div>
+   <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styleUrls: ['./app.component.css']
 })
@@ -41,18 +43,7 @@ export class AppComponent {
   }
 }
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+
 
 
 
